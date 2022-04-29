cb54045c9577535fafdfe9eaa98e8e6aa794936e
alessonmoura <alessoncm@gmail.com>
2022-04-28 22:41:30 -0300

    primeiro-script

---
+++ 01-primeiro-script/main.tf
@@ -0,0 +1,30 @@
+terraform{
+  required_version = "1.1.9"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    profile = "terraform"
+}
+
+resource "aws_s3_bucket" "alesson-primeiro-script" {
+  bucket = "alesson-primeiro-script"
+
+  tags = {
+    Name        = "alesson-primeiro-script"
+    Environment = "Dev"
+    Managedby = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_acl" "alesson-primeiro-script" {
+  bucket = aws_s3_bucket.alesson-primeiro-script.id
+  acl    = "private"
+}
