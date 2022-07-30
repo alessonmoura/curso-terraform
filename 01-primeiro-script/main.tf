743ed6b1889793b9049df089a40bb6f9e36011f1
alessonmoura <alessoncm@gmail.com>
2022-07-29 23:27:35 -0300

    create bucket estudos

--- 01-primeiro-script/main.tf
+++ 01-primeiro-script/main.tf
@@ -1,10 +1,10 @@
 terraform{
-  required_version = "1.1.9"
+  required_version = "1.2.6"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.11.0"
+      version = "4.24.0"
     }
   }
 }
